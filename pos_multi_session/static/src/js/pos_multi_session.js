0c484b8458366d05122e0026de588ec86ed9bdd6
florent.thomas <florent.thomas@mind-and-go.com>
2016-05-09 22:23:46 +0200

    Synchronize note between sessions

--- pos_multi_session/static/src/js/pos_multi_session.js
+++ pos_multi_session/static/src/js/pos_multi_session.js
@@ -233,7 +233,10 @@ odoo.define('pos_multi_session', function(require){
                 }
                 if(dline.discount !== undefined){
                     line.set_discount(dline.discount);
-                }
+                }                
+                if(dline.note !== ""){
+                    line.note = dline.note;
+                }               
                 order.orderlines.add(line)
             })
 
